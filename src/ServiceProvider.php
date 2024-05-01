cb59b58df943fa99de3145910468f1c39198f5d3
Ben @ Netnak <ben@netnak.co.uk>
2024-05-01 21:45:57 +0100

    Add modifier to serviceprovider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,10 +2,15 @@
 
 namespace Netnak\Tel;
 
+use Netnak\Tel\Modifiers\Tel;
 use Statamic\Providers\AddonServiceProvider;
 
 class ServiceProvider extends AddonServiceProvider
 {
+    protected $modifiers = [
+        Tel::class
+    ];
+    
     public function bootAddon()
     {
         //
